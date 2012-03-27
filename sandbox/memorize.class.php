b96365cfd6bd1542607f0e430eff1d9555bc5910
cbrghost@gmail.com <cbrghost@gmail.com@2d502fbb-17fd-4f66-c0ea-378795328542>
2012-03-27 11:15:17 +0000

    코드 리팩토링(업데이트체크, 업데이트에서 트리거 삭제)

--- sandbox/memorize.class.php
+++ sandbox/memorize.class.php
@@ -7,11 +7,11 @@
 
 	class memorize extends ModuleObject 
 	{
-	
+
 		/**
 		* @brief 설치시 추가 작업이 필요할시 구현
 		**/
-		function moduleInstall() 
+		function moduleInstall()
 		{
 			$oModuleController = &getController('module');
 			$oModuleController->insertTrigger('module.dispAdditionSetup', 'memorize', 'view', 'triggerDispMemorizeAdditionSetup', 'before');
@@ -22,24 +22,15 @@
 			$oModuleController->insertTrigger('comment.deleteComment', 'memorize', 'controller', 'triggerDeleteComment', 'before');
 			$oModuleController->insertTrigger('file.deleteFile','memorize', 'controller', 'triggerDeleteFile', 'before');
 			$oModuleController->insertTrigger('display', 'memorize', 'controller', 'triggerDisplay','before');
+
 			return new Object();
 		}
 
 		/**
 		 * @brief 설치가 이상이 없는지 체크하는 method
 		 **/
-		function checkUpdate() 
+		function checkUpdate()
 		{
-			$oModuleModel = &getModel('module');
-			if(!$oModuleModel->getTrigger('module.dispAdditionSetup', 'memorize', 'view', 'triggerDispMemorizeAdditionSetup', 'before')) return true;
-			if(!$oModuleModel->getTrigger('module.deleteModule', 'memorize', 'controller', 'triggerDeleteModule', 'before')) return true;
-			if(!$oModuleModel->getTrigger('document.updateDocument', 'memorize', 'controller', 'triggerUpdateDocument', 'before')) return true;
-			if(!$oModuleModel->getTrigger('document.deleteDocument', 'memorize', 'controller', 'triggerDeleteDocument', 'before')) return true;
-			if(!$oModuleModel->getTrigger('comment.updateCommment', 'memorize', 'controller', 'triggerUpdateComment', 'before')) return true;
-			if(!$oModuleModel->getTrigger('comment.deleteComment', 'memorize', 'controller', 'triggerDeleteComment', 'before')) return true;
-			if(!$oModuleModel->getTrigger('file.deleteFile','memorize', 'controller', 'triggerDeleteFile', 'before')) return true;
-			if(!$oModuleModel->getTrigger('display', 'memorize', 'controller', 'triggerDisplay','before')) return true;
-			return false;
 		}
 
 		/**
@@ -47,19 +38,8 @@
 		 **/
 		function moduleUpdate()
 		{
-			$oModuleModel = &getModel('module');
-			$oModuleController = &getController('module');
-			if(!$oModuleModel->getTrigger('module.dispAdditionSetup', 'memorize', 'view', 'triggerDispMemorizeAdditionSetup', 'before')) $oModuleController->insertTrigger('module.dispAdditionSetup', 'memorize', 'view', 'triggerDispMemorizeAdditionSetup', 'before');
-			if(!$oModuleModel->getTrigger('module.deleteModule', 'memorize', 'controller', 'triggerDeleteModule', 'before')) $oModuleController->insertTrigger('module.deleteModule', 'memorize', 'controller', 'triggerDeleteModule', 'before');
-			if(!$oModuleModel->getTrigger('document.updateDocument', 'memorize', 'controller', 'triggerUpdateDocument', 'before')) $oModuleController->insertTrigger('document.updateDocument', 'memorize', 'controller', 'triggerUpdateDocument', 'before');
-			if(!$oModuleModel->getTrigger('document.deleteDocument', 'memorize', 'controller', 'triggerDeleteDocument', 'before')) $oModuleController->insertTrigger('document.deleteDocument', 'memorize', 'controller', 'triggerDeleteDocument', 'before');;
-			if(!$oModuleModel->getTrigger('comment.updateCommment', 'memorize', 'controller', 'triggerUpdateComment', 'before')) $oModuleController->insertTrigger('comment.updateCommment', 'memorize', 'controller', 'triggerUpdateComment', 'before');
-			if(!$oModuleModel->getTrigger('comment.deleteComment', 'memorize', 'controller', 'triggerDeleteComment', 'before')) $oModuleController->insertTrigger('comment.deleteComment', 'memorize', 'controller', 'triggerDeleteComment', 'before');
-			if(!$oModuleModel->getTrigger('file.deleteFile','memorize', 'controller', 'triggerDeleteFile', 'before')) $oModuleController->insertTrigger('file.deleteFile','memorize', 'controller', 'triggerDeleteFile', 'before');
-			if(!$oModuleModel->getTrigger('display', 'memorize', 'controller', 'triggerDisplay','before')) $oModuleController->insertTrigger('display', 'memorize', 'controller', 'triggerDisplay','before');
-			return new Object(0, 'success_updated');
 		}
-		
+
 		/**
 		* @brief 모듈 삭제 실행
 		**/
@@ -67,17 +47,49 @@
 		{
 			$oModuleModel = &getModel('module');
 			$oModuleController = &getController('module');
-			if($oModuleModel->getTrigger('module.dispAdditionSetup', 'memorize', 'view', 'triggerDispMemorizeAdditionSetup', 'before')) $oModuleController->deleteTrigger('module.dispAdditionSetup', 'memorize', 'view', 'triggerDispMemorizeAdditionSetup', 'before');
-			if($oModuleModel->getTrigger('module.deleteModule', 'memorize', 'controller', 'triggerDeleteModule', 'before')) $oModuleController->deleteTrigger('module.deleteModule', 'memorize', 'controller', 'triggerDeleteModule', 'before');
-			if($oModuleModel->getTrigger('document.updateDocument', 'memorize', 'controller', 'triggerUpdateDocument', 'before')) $oModuleController->deleteTrigger('document.updateDocument', 'memorize', 'controller', 'triggerUpdateDocument', 'before');
-			if($oModuleModel->getTrigger('document.deleteDocument', 'memorize', 'controller', 'triggerDeleteDocument', 'before')) $oModuleController->deleteTrigger('document.deleteDocument', 'memorize', 'controller', 'triggerDeleteDocument', 'before');;
-			if($oModuleModel->getTrigger('comment.updateCommment', 'memorize', 'controller', 'triggerUpdateComment', 'before')) $oModuleController->deleteTrigger('comment.updateCommment', 'memorize', 'controller', 'triggerUpdateComment', 'before');
-			if($oModuleModel->getTrigger('comment.deleteComment', 'memorize', 'controller', 'triggerDeleteComment', 'before')) $oModuleController->deleteTrigger('comment.deleteComment', 'memorize', 'controller', 'triggerDeleteComment', 'before');
-			if($oModuleModel->getTrigger('file.deleteFile','memorize', 'controller', 'triggerDeleteFile', 'before')) $oModuleController->deleteTrigger('file.deleteFile','memorize', 'controller', 'triggerDeleteFile', 'before');
-			if($oModuleModel->getTrigger('display', 'memorize', 'controller', 'triggerDisplay','before')) $oModuleController->deleteTrigger('display', 'memorize', 'controller', 'triggerDisplay','before');
+
+			if($oModuleModel->getTrigger('module.dispAdditionSetup', 'memorize', 'view', 'triggerDispMemorizeAdditionSetup', 'before'))
+			{
+				$oModuleController->deleteTrigger('module.dispAdditionSetup', 'memorize', 'view', 'triggerDispMemorizeAdditionSetup', 'before');
+			}
+
+			if($oModuleModel->getTrigger('module.deleteModule', 'memorize', 'controller', 'triggerDeleteModule', 'before'))
+			{
+				$oModuleController->deleteTrigger('module.deleteModule', 'memorize', 'controller', 'triggerDeleteModule', 'before');
+			}
+
+			if($oModuleModel->getTrigger('document.updateDocument', 'memorize', 'controller', 'triggerUpdateDocument', 'before'))
+			{
+				$oModuleController->deleteTrigger('document.updateDocument', 'memorize', 'controller', 'triggerUpdateDocument', 'before');
+			}
+
+			if($oModuleModel->getTrigger('document.deleteDocument', 'memorize', 'controller', 'triggerDeleteDocument', 'before'))
+			{
+				$oModuleController->deleteTrigger('document.deleteDocument', 'memorize', 'controller', 'triggerDeleteDocument', 'before');;
+			}
+
+			if($oModuleModel->getTrigger('comment.updateCommment', 'memorize', 'controller', 'triggerUpdateComment', 'before'))
+			{
+				$oModuleController->deleteTrigger('comment.updateCommment', 'memorize', 'controller', 'triggerUpdateComment', 'before');
+			}
+
+			if($oModuleModel->getTrigger('comment.deleteComment', 'memorize', 'controller', 'triggerDeleteComment', 'before'))
+			{
+				$oModuleController->deleteTrigger('comment.deleteComment', 'memorize', 'controller', 'triggerDeleteComment', 'before');
+			}
+
+			if($oModuleModel->getTrigger('file.deleteFile','memorize', 'controller', 'triggerDeleteFile', 'before'))
+			{
+				$oModuleController->deleteTrigger('file.deleteFile','memorize', 'controller', 'triggerDeleteFile', 'before');
+			}
+
+			if($oModuleModel->getTrigger('display', 'memorize', 'controller', 'triggerDisplay','before'))
+			{
+				$oModuleController->deleteTrigger('display', 'memorize', 'controller', 'triggerDisplay','before');
+			}
+
 			return new Object();
 		}
-
 
 		/**
 		 * @brief 캐시 파일 재생성
