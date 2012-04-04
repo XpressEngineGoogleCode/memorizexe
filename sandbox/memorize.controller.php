5f2c968fa4a782e56717cc459b5f8570ee06993f
songmea <songmea@2d502fbb-17fd-4f66-c0ea-378795328542>
2012-04-04 05:37:16 +0000

    Issue 17 triggerUpdateComment()추가

--- sandbox/memorize.controller.php
+++ sandbox/memorize.controller.php
@@ -453,6 +453,122 @@
 		 **/
 		function triggerUpdateComment(&$obj)
 		{
+			if(!$obj->module_srl || !$obj->document_srl || !$obj->comment_srl)
+			{
+				return;
+			}
+
+			$module_srl = $obj->module_srl;
+
+			//module_srl에서 module_info 가져오기
+			$oModuleModel = &getModel('module');
+			$module_info = $oModuleModel->getModuleInfoByModuleSrl($module_srl);
+			$module = $module_info->module;
+
+			// 비교결과를 담는다.
+			$is_diff = NULL;
+
+			// 기록할 확장변수의 정보를 담는다.
+			$args_diff = NULL;
+
+			// 넘어온 값을 우선 배열에 담는다.
+			$args = $obj;
+
+			// 모듈 설정을 가져옵니다.
+			$oMemorizeModel = &getModel('memorize');
+			$memorize_config = $oMemorizeModel->getMemorizeConfig($module, $module_srl);
+			if($memorize_config->use_update_comment != 'Y')
+			{
+				return;
+			}
+
+			$document_srl = $obj->document_srl;
+			$comment_srl = $obj->comment_srl;
+
+			$oCommentModel = &getModel('comment');
+			$oComment = $oCommentModel->getComment($comment_srl,false,false);
+
+			$oldComment = $oComment->variables;
+			$logged_info = Context::get('logged_info');
+
+			//관리자가 아닐경우 HackTag 제거
+			if($logged_info->is_admin != 'Y')
+			{
+				$args->content = removeHackTag($obj->content);
+			}
+
+			if(!isset($obj->notify_message))
+			{
+				$args->notify_message = 'N';
+			}
+
+			if(!isset($obj->is_secret))
+			{
+				$args->is_secret = 'N';
+			}
+
+			if(!isset($obj->nick_name))
+			{
+				$args->nick_name = $logged_info->nick_name;
+			}
+
+			// 홈페이지 주소
+			if(!isset($obj->homepage))
+			{
+				$args->homepage = '';
+			}
+			elseif(!preg_match('/^[a-z]+:\/\//i', $obj->homepage))
+			{
+				$args->homepage = "http://{$obj->homepage}";
+			}
+
+			/*
+			 * 이전 저장된 comment의 내용과 비교한다.
+			*/
+			foreach($oldComment as $key => $val)
+			{
+				// 비교 대상이 되는 컬럼 값
+				if(!in_array($key, array('is_secret','content','notify_message','password','email_address','homepage','ipaddress','nick_name')))
+				{
+					continue;
+				}
+
+				// 비교하려는 변수가 존재하고, 비교 결과가 다르다면 해당 컬럼을 배열에 담는다.
+				if(isset($obj->{$key}) && $val != $obj->{$key})
+				{
+					$is_diff->{$key} = TRUE;
+				}
+			}
+
+			// 비교한 컬럼 정보와 함께 xe_comments의 글을 기록한다.
+			if(count($is_diff) >= 1)
+			{
+				// type의 글 수정에 대한 수행번호를 선언
+				$args->type = $this->memorize_type['comment'];
+				// 마지막 글의 idx를 가져와서 양수로 바꾼 후 1을 더한 다음, 다시 음수로 바꿉니다.
+				$args->idx = $oMemorizeModel->getMemorizeLastIdx($comment_srl);
+				$args->module_srl = $module_srl;
+				$args->content_srl = $comment_srl;
+				$args->content2 = $oldComment['content'];
+
+				// extra_vars는 데이터 타입이 text이기 때문에 bigtext 타입의 본문은 제거 합니다.
+				unset($oldComment['content']);
+				$args->extra_vars = serialize($oldComment);
+
+				// 수정시 기존에 등록되었던 글을 기록 합니다.
+				$oMemorizeDatas = $this->insertMemorizeDatas($args);
+				$memory_srl = $oMemorizeDatas->variables['memory_srl'];
+
+				// 기록된 글의 sequence번호를 기록
+				$args->memory_srl = $memory_srl;
+				// 로그 기록 형식을 수정사항으로 기록
+				$args->code = $this->memorize_code['update'];
+				// 비교 결과 값이 다른 컬럼을 정리한다.
+				$args->diff_column = serialize($is_diff);
+
+				// 수정시 로그를 기록합니다.
+				$this->insertMemorizeLog($args);
+			}
 		}
 
 		/**
